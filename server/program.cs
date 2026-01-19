6fddc1f12403bf0559d4967ca0ae6b21a9b9be82
Sliceeee01 <ivan_hih@icloud.com>
2026-01-19 16:27:55 +0300

    commit 10

--- server/program.cs
+++ server/program.cs
@@ -4,3 +4,8 @@ Console.WriteLine("Богданов Иван Антонович");
 Console.WriteLine("ИСП-233");
 Console.WriteLine("19.01.2026");
 Console.WriteLine("16:23");
+Console.WriteLine("Меню: ");
+Console.WriteLine("1 —Показать ФИО");
+Console.WriteLine("2 —Показать группу");
+Console.WriteLine("3 —Показать дату");
+Console.WriteLine("4 —Выход");
