c8d6a296534a6854b32c235a1f7087babe5d002a
Sliceeee01 <ivan_hih@icloud.com>
2026-01-19 16:24:28 +0300

    commit 9

--- server/program.cs
+++ server/program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Привет!!!!!!!!");
 Console.WriteLine("Богданов Иван Антонович");
+Console.WriteLine("ИСП-233");
+Console.WriteLine("19.01.2026");
+Console.WriteLine("16:23");
